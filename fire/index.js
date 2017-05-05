8907e8ddadb0327185b61beda8e55e40dcfd7d0d
Sarah Herr <sarah.herr@hotmail.com>
2017-05-05 15:36:51 -0400

    setup(firebones)

--- fire/index.js
+++ fire/index.js
@@ -2,12 +2,12 @@ const firebase = require('firebase')
 
 // -- // -- // -- // Firebase Config // -- // -- // -- //
 const config = {
-  apiKey: 'AIzaSyBzz-Wq2dzMgM7E8cdSYnYoX5fbVUT-XQo',
-  authDomain: 'firebones-6bc2a.firebaseapp.com',
-  databaseURL: 'https://firebones-6bc2a.firebaseio.com',
-  projectId: 'firebones-6bc2a',
-  storageBucket: 'firebones-6bc2a.appspot.com',
-  messagingSenderId: '1030378391678'
+  apiKey: 'AIzaSyD6m_QmW1R8w6bBu1R1qnhnDqCNWMGS4Es',
+  authDomain: 'capstone-c9e61.firebaseapp.com',
+  databaseURL: 'https://capstone-c9e61.firebaseio.com',
+  projectId: 'capstone-c9e61',
+  storageBucket: 'capstone-c9e61.appspot.com',
+  messagingSenderId: '395040208448'
 }
 // -- // -- // -- // -- // -- // -- // -- // -- // -- //
 
