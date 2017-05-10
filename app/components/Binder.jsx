2ea55c40a5a25925ed619f0725a4909a344cf17b
Sarah Herr <sarah.herr@hotmail.com>
2017-05-10 09:48:33 -0400

    fix(Binder): changed functions to class properties

--- app/components/Binder.jsx
+++ app/components/Binder.jsx
@@ -7,8 +7,6 @@ export default class extends React.Component {
       showInput: false,
       newAtom: ''
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleSelect = this.handleSelect.bind(this)
   }
 
   // this is class property so we don't have to bind it (as if we put this entire function in the constructor)
@@ -17,13 +15,13 @@ export default class extends React.Component {
     this.setState({ newContainer: evt.target.value })
   }
 
-  handleSubmit(evt) {
+  handleSubmit = (evt) => {
     evt.preventDefault()
     console.log('this.state.newAtom in handleSubmit in Binder.jsx', this.state.newAtom)
     // this.props.createContainer({ title: this.state.newContainer })
   }
 
-  handleSelect(evt) {
+  handleSelect = (evt) => {
     evt.preventDefault()
     console.log('evt.target.value in handleSelect in Binder.jsx', evt.target.value)
     // this.props.selectItem(evt.target.value)
