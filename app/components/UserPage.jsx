56600ee1bfd517fb36ed8ca0e877149e68cf0db1
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-05-11 22:58:38 -0400

    feat(comments): to explain userpage

--- app/components/UserPage.jsx
+++ app/components/UserPage.jsx
@@ -35,6 +35,7 @@ export default class extends React.Component {
   }
 
   createProject = () => {
+    // create project object to add to projects db
     const project = {
       projectTitle: this.state.newProjectName,
       current: {
@@ -48,21 +49,29 @@ export default class extends React.Component {
       }
     }
 
+    // create a new 'key' under 'projects' db
     const projectKey = firebase.database().ref('projects').push().key
+
+    // create updates object
     const updates = {}
+    // update projects db with new project
     updates['/projects/' + projectKey] = project
+    // update users db at the current user's object with new {project key: project key} object
     updates['/users/' + this.props.user.uid + '/projects/' + projectKey] = projectKey
     return firebase.database().ref().update(updates)
   }
 
   createUser = () => {
+    // create new user object
     const user = {
       name: this.props.user.displayName,
       email: this.props.user.email,
       uid: this.props.user.uid
     }
 
+    // finding user by current user's uid
     firebase.database().ref('users').orderByChild('uid').equalTo(this.props.user.uid).on('value', snapshot => {
+      // if there is no user with current user's uid, set create new user in users db
       if (!snapshot.val()) {
         firebase.database().ref('/users/' + this.props.user.uid).set(user)
       }
@@ -79,6 +88,7 @@ export default class extends React.Component {
       projectKeys.forEach(projectKey => {
         firebase.database().ref('projects').child(projectKey).on('value', snapshot => {
           const currentProject = snapshot.val()
+          // add each project title into the projectsList - MUST BE DONE THIS WAY TO UPDATE STATE
           this.setState({projectList: [...this.state.projectList, currentProject.projectTitle]})
         })
       })
@@ -99,7 +109,7 @@ export default class extends React.Component {
         </form>
         <h3>Pick a project to view:</h3>
         <select>
-        <option>       </option>
+        <option> </option>
         {this.state.projectList.map(project => <option key={project}>{project}</option>)}
         </select>
         <button type='button' onClick={this.goToPage}>Go to project</button>
