a07f1c4ac5bf844c13fddbed13195d2cbeeb4ee1
Kate Caldwell <kate.kec.caldwell@gmail.com>
2017-05-16 17:36:25 -0400

    fix(binder)

--- app/components/Tree.jsx
+++ app/components/Tree.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router'
-import {atomRef} from 'APP/server/db/model'
+import {atomRef, projectRef} from 'APP/server/db/model'
 
 export default class Tree extends React.Component {
   componentDidMount() {
@@ -23,6 +23,20 @@ export default class Tree extends React.Component {
     this.setState({expanded: !this.state.expanded})
   }
 
+  handleChange = (evt) => {
+    this.setState({newAtom: evt.target.value})
+  }
+  add = () => {
+    const newAtom = {
+      title: this.state.newAtom
+    }
+    const atoms = projectRef(this.props.projectId).child('atoms')
+    const newAtomKey = atoms.push().key
+    atomRef(this.props.projectId, this.props.atomId).child('children').child(newAtomKey).set(true)
+    atomRef(this.props.projectId, newAtomKey).set(newAtom)
+    // atoms.child(newAtomKey).set(newAtom)
+  }
+
   delete = () => {
     atomRef(this.props.projectId, this.props.atomId).remove()
   }
@@ -30,15 +44,15 @@ export default class Tree extends React.Component {
   render() {
     if (!this.state || !this.state.value) return null
     const {value: {children, title}, expanded} = this.state || {value: {}}
+    const iconClass = children ? (expanded ? 'chevron-down' : 'chevron-right') : 'file-text-o'
     return (
       <li>
-        <span onClick={this.toggle}>{expanded ? '-' : '+'}</span>
-        <Link to={`/${this.props.uid}/project/${this.props.projectId}/${this.props.atomId}`}>{title}</Link><span onClick={this.delete}>x</span>
-        <ul>{
-          expanded && Object.keys(children || {}).map(childId => <Tree projectId={this.props.projectId} atomId={childId} /> ).concat([<button>Add</button>])
+        <span className={`fa fa-${iconClass}`}onClick={this.toggle} />
+        <Link to={`/${this.props.uid}/project/${this.props.projectId}/${this.props.atomId}`} className='binder-item'>{title}</Link><span className='fa fa-times' onClick={this.delete} />
+        <ul className='binder-list'>{
+          expanded && Object.keys(children || {}).map(childId => <Tree uid={this.props.uid} projectId={this.props.projectId} atomId={childId} />).concat([<span><input onChange={this.handleChange} /><span onClick={this.add}className='fa fa-plus'/></span>])
         }</ul>
       </li>
     )
-    
   }
 }
--- server/db/model.js
+++ server/db/model.js
@@ -2,9 +2,9 @@ const firebase = require('firebase')
 const db = firebase.database()
 
 const atomRef = (projectId, atomId) =>
-    projectRef(projectId).child('current').child('atoms').child(atomId)
+    projectRef(projectId).child('atoms').child(atomId)
 
 const projectRef = (projectId) =>
-    db.ref('projects').child(projectId)
+    db.ref('projects').child(projectId).child('current')
 
 module.exports = { atomRef, projectRef }
