fae9747ae8be9f03ba8dad7d9cefbcd1c6428c9f
Sarah Herr <sarah.herr@hotmail.com>
2017-05-16 20:06:33 -0400

    feat(scroll): added scroll back into the Binder

--- app/components/Binder.jsx
+++ app/components/Binder.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Tree from './Tree'
+import Infinite from 'react-infinite'
 
 const Binder = (props) =>
   <div className='panel'>
     <div className='panel-heading'>
       <h3 className='panel-head'>Binder</h3>
     </div>
-    <div className='panel-body'>
-      <ul id='binder' className='binder-list'><Tree uid={props.uid} projectId={props.projectId} atomId='0' /></ul>
-    </div>
+    <Infinite containerHeight={400} elementHeight={26}>
+      <div className='panel-body'>
+        <ul id='binder' className='binder-list'><Tree uid={props.uid} projectId={props.projectId} atomId='0' /></ul>
+      </div>
+    </Infinite>
   </div>
-
 
 export default Binder
